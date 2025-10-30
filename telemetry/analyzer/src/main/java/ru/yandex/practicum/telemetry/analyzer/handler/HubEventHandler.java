68953a6a8ae9ba8768cd8dd8c620a568a15c4890
Ekaterina <e.fayzulina@inbox.ru>
2025-10-31 00:36:45 +0700

    Update analyzer

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/handler/HubEventHandler.java
@@ -0,0 +1,9 @@
+//package ru.yandex.practicum.telemetry.analyzer.handler;
+//
+//import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+//
+//public interface HubEventHandler {
+//    Class<?> getPayloadClass();
+//
+//    void handle(HubEventAvro event);
+//}
